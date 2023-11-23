167c8b9ce430e2c7550f50d26d399aed94906822
Justin Cheung <jrcheung168@gmail.com>
2023-11-22 20:03:17 -0600

    try other way around

--- client.py
+++ client.py
@@ -1,43 +1,45 @@
 #!/usr/bin/env python3
 import socket
 
-host = '192.168.1.191'# ip of raspberry pi 
-port = 12345
+# host = '192.168.1.191'# ip of raspberry pi 
+# port = 12345
 
 
-s = socket.socket()
-s.connect((host, port))
+# s = socket.socket()
+# s.connect((host, port))
 
-try:
-    while True:
-        message = input(" --> ")
-        s.send(message.encode())
-except KeyboardInterrupt:
-    print("\nExiting...")
-
-s.close()
-
-
-# def client_program():
-#     # host = socket.gethostname()  # as both code is running on same pc
-#     host = '192.168.1.191'
-#     port = 12345  # socket server port number
-
-#     client_socket = socket.socket()  # instantiate
-#     client_socket.connect((host, port))  # connect to the server
-
-#     message = input(" -> ")  # take input
-
-#     while message.lower().strip() != 'bye':
-#         client_socket.send(message.encode())  # send message
-#         data = client_socket.recv(1024).decode()  # receive response
-
-#         print('Received from server: ' + data)  # show in terminal
-
-#         message = input(" -> ")  # again take input
-
-#     client_socket.close()  # close the connection
+# try:
+#     while True:
+#         message = input(" --> ")
+#         s.send(message.encode())
+# except KeyboardInterrupt:
+#     print("\nExiting...")
+#     s.close()
 
 
-# if __name__ == '__main__':
-#     client_program()
+
+
+
+def client_program():
+    # host = socket.gethostname()  # as both code is running on same pc
+    host = '192.168.1.191'
+    port = 12345  # socket server port number
+
+    client_socket = socket.socket()  # instantiate
+    client_socket.connect((host, port))  # connect to the server
+
+    message = input(" -> ")  # take input
+
+    while message.lower().strip() != 'bye':
+        client_socket.send(message.encode())  # send message
+        data = client_socket.recv(1024).decode()  # receive response
+
+        print('Received from server: ' + data)  # show in terminal
+
+        message = input(" -> ")  # again take input
+
+    client_socket.close()  # close the connection
+
+
+if __name__ == '__main__':
+    client_program()
--- server.py
+++ server.py
@@ -1,50 +1,52 @@
 #!/usr/bin/env python3
 import socket
 
-s = socket.socket()
-host = '192.168.1.191' #ip of raspberry pi
-port = 12345
-s.bind((host, port))
+# s = socket.socket()
+# host = '192.168.1.191' #ip of raspberry pi
+# port = 12345
+# s.bind((host, port))
 
-# Allow 1 client
-s.listen(1)
-c, addr = s.accept()
-print(f'Got connection from: {addr}')
-c.send(b'Connected to host')
+# # Allow 1 client
+# s.listen(1)
+# c, addr = s.accept()
+# print(f'Got connection from: {addr}')
+# c.send(b'Connected to host')
 
-while True:
-    data = c.recv(1024).decode()
-    if not data:
-        break
-    print(f'Received: {data}')
+# while True:
+#     data = c.recv(1024).decode()
+#     if not data:
+#         break
+#     print(f'Received: {data}')
 
-c.close()
-
-
-# def server_program():
-#     # get the hostname
-#     host = "192.168.1.191"
-#     port = 12345  # initiate port no above 1024
-
-#     server_socket = socket.socket()  # get instance
-#     server_socket.bind((host, port))  # bind host address and port together
-
-#     # configure how many client the server can listen simultaneously
-#     server_socket.listen(2)
-#     conn, address = server_socket.accept()  # accept new connection
-#     print("Connection from: " + str(address))
-#     while True:
-#         # receive data stream. it won't accept data packet greater than 1024 bytes
-#         data = conn.recv(1024).decode()
-#         if not data:
-#             # if data is not received break
-#             break
-#         print("from connected user: " + str(data))
-#         data = input(' -> ')
-#         conn.send(data.encode())  # send data to the client
-
-#     conn.close()  # close the connection
+# c.close()
 
 
-# if __name__ == '__main__':
-#     server_program()
+
+
+def server_program():
+    # get the hostname
+    host = "192.168.1.191"
+    port = 12345  # initiate port no above 1024
+
+    server_socket = socket.socket()  # get instance
+    server_socket.bind((host, port))  # bind host address and port together
+
+    # configure how many client the server can listen simultaneously
+    server_socket.listen(2)
+    conn, address = server_socket.accept()  # accept new connection
+    print("Connection from: " + str(address))
+    while True:
+        # receive data stream. it won't accept data packet greater than 1024 bytes
+        data = conn.recv(1024).decode()
+        if not data:
+            # if data is not received break
+            break
+        print("from connected user: " + str(data))
+        data = input(' -> ')
+        conn.send(data.encode())  # send data to the client
+
+    conn.close()  # close the connection
+
+
+if __name__ == '__main__':
+    server_program()
