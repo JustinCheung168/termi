7ddf42aac8ab2eb9747ea7afa80d43c18a0878e8
Justin Cheung <jrcheung168@gmail.com>
2023-11-22 20:15:08 -0600

    try catching ctrl c

--- server.py
+++ server.py
@@ -8,22 +8,25 @@ def server_program():
 
     server_socket = socket.socket()  # get instance
     server_socket.bind((host, port))  # bind host address and port together
+    server_socket.settimeout(None) # socket can wait indefinitely
 
     # configure how many client the server can listen simultaneously
     server_socket.listen(1)
     conn, address = server_socket.accept()  # accept new connection
     print("Connection from: " + str(address))
-    while True:
-        # receive data stream. it won't accept data packet greater than 1024 bytes
-        data = conn.recv(1024).decode()
-        if not data:
-            # if data is not received break
-            break
-        print("from connected user: " + str(data))
-        # data = input(' -> ')
-        # conn.send(data.encode())  # send data to the client
-
-    conn.close()  # close the connection
+    try:
+        while True:
+            # receive data stream. it won't accept data packet greater than 1024 bytes
+            data = conn.recv(1024).decode()
+            if not data:
+                # if data is not received break
+                break
+            print("from connected user: " + str(data))
+        print("Lost connection")
+        conn.close()  # close the connection
+    except KeyboardInterrupt:
+        print("Quitting")
+        conn.close()  # close the connection
 
 
 if __name__ == '__main__':
